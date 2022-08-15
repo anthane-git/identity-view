ed998ee852a4cbf0dbedc362ffdeeb9e8797da3e
BlazeIsClone <64116737+BlazeIsClone@users.noreply.github.com>
2022-08-15 12:09:16 +0530

    update env entrypoint

--- src/services/endpoints.ts
+++ src/services/endpoints.ts
@@ -1,5 +1,5 @@
-export const localRegisterRoute = '/v1/auth/local/register';
-export const localLoginRoute = '/v1/auth/local/login';
-export const logoutRoute = '/v1/auth/logout';
-export const refreshTokenRoute = '/v1/auth/refresh-token';
-export const protectedRoute = '/v1/users/me';
+export const localRegisterRoute = '/auth/local/register';
+export const localLoginRoute = '/auth/local/login';
+export const logoutRoute = '/auth/logout';
+export const refreshTokenRoute = '/auth/refresh-token';
+export const protectedRoute = '/users/me';
